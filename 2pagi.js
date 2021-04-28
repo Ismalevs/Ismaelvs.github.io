c34d96fa1eae9c1028e9caa281dcae7b63fa78ae
Ismael <IsmaleVS>
2021-04-27 22:07:08 -0500

    Version final

---
+++ 2pagi.js
@@ -0,0 +1,19 @@
+console.log('hola que paso ')
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+   document.querySelector('.nav-menu').classList.toggle(show)
+});
+
+
+
+
+// //otro comentario
+// var nombre="Ismael";
+// var edad =24;
+
+// document.write(nombre+""+edad);
+// alert("hola");
+// // prompt("hola");
+
+// function hola_Isma(){
+//     console.log('isma,yutuvw')
+// }
--- 2pagiVideo.js
+++
@@ -1,7 +0,0 @@
-//otro comentario
-var nombre="Ismael";
-var edad =24;
-
-document.write(nombre+""+edad);
-alert("hola");
-// prompt("hola");
